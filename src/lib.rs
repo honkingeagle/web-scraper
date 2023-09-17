ec810c8288f1637f32e2452f2e4f6a7f8892031f
Tiras Mwangi <tmwangi599@gmail.com>
2023-09-17 19:38:42 +0300

    Parses html document from request

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+mod scrape;
+mod parse_product;
+pub mod build_csv;
+
+use parse_product::parse_product;
+use scraper::{Html, Selector};
+
+#[derive(Debug)]
+pub struct Product {
+    pub url: String,
+    pub image: String,
+    pub name: String,
+    pub price: String,
+}
+
+pub async fn parse_document(html: String) {
+    let document = Html::parse_document(&html);
+    let li_selector = Selector::parse("li.product").unwrap();
+    let html_products = document.select(&li_selector);
+
+    parse_product(html_products);
+}
