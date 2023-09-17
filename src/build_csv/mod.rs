a987f8e175c88cb79a3cf3dd91790c14eb9e2a68
Tiras Mwangi <tmwangi599@gmail.com>
2023-09-17 21:47:24 +0300

    The following changes have been made: -> added test that inserts a Product to a csv file -> serialized all products in Vec<Product>, created a products.csv file and inserted the products there.

--- src/build_csv/mod.rs
+++ src/build_csv/mod.rs
@@ -1,16 +1,37 @@
 use crate::Product;
-
+use csv::Writer;
 pub fn build_csv_file(products: Vec<Product>) {
-    println!("{:?}", products);
+    let mut wtr = Writer::from_path("products.csv").unwrap();
+    for product in products {
+        wtr.serialize(product).unwrap_or_else(|err| {
+            println!("Unable to serialize product: {}", err);
+        });
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::Product;
     use csv::Writer;
 
     #[test]
+    #[ignore]
     fn csv_writer_creates_file() {
         let wtr = Writer::from_path("products.csv");
         assert!(wtr.is_ok());
+    }
+
+    #[test]
+    #[ignore]
+    fn csv_writer_create_headers_example() {
+        let product = Product {
+            url: "https://example.com".to_string(),
+            image: "https://imageurl/product.png".to_string(),
+            name: "Pikachu".to_string(),
+            price: "100".to_string()
+        };
+        
+        let mut wtr = Writer::from_path("products.csv").unwrap();
+        assert!(wtr.serialize(product).is_ok());
     }
 }
